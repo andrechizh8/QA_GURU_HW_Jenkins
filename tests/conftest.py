3b977bdb61c7573883ed6eebaf15e3855cf052cf
andrechizh8 <andrechizh.ru@yandex.ru>
2023-01-18 13:11:33 +0300

    add selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@ def browser_setup():
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
